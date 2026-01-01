8d90094a7287a319dab227d5c782f8b0948eac47
Pursharth Gujral <pursharth.gujral@verifavia-shipping.com>
2026-01-01 17:03:32 +0530

    feat: add structured logging with Serilog

--- Backend/ERWMS.Api/ERWMS.Api/Program.cs
+++ Backend/ERWMS.Api/ERWMS.Api/Program.cs
@@ -1,16 +1,24 @@
 using ERWMS.Api.Middlewares;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.Console()
+	.WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+	.CreateLogger();
+
+builder.Host.UseSerilog();
+
 var app = builder.Build();
 app.UseMiddleware<GlobalExceptionMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
